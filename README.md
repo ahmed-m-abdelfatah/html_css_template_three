5d19e29ca933f0db51b74ecb4d96a13255daaf66
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2021-10-30 03:46:56 +0200

    some edit

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 1. https://www.youtube.com/playlist?list=PLDoPjvoNmBAxuCSp2_-9LurPqRVwketnc
 2. https://youtu.be/eFsiOTJrrE8
 3. https://youtu.be/sbIoIKI9FOc
+4. https://youtu.be/PLsUdgLnzgQ
+5. https://github.com/bradtraversy/50projects50days/tree/master/incrementing-counter
 
 ## view live
 
--- js/main.js
+++ js/main.js
@@ -2,57 +2,46 @@
 let statsSection = document.querySelector('.stats');
 let counters = document.querySelectorAll('.stats .container .box .number');
 
-let started = false;
-window.onscroll = function () {
-  if (window.scrollY >= statsSection.offsetTop - 300) {
-    if (!started) {
-      counters.forEach(counter => startCount(counter));
+counters.forEach((counter, index) => {
+  console.log('call counters.forEach');
+
+  // counter.textContent = '0';
+
+  // JavaScript - What is Hoisting ? https://youtu.be/3nswTRsN5XA
+  let updateCounter = () => {
+    // + to number
+    // https://stackoverflow.com/a/1133814/16107539
+    let target = +counter.dataset.target;
+    let nowCounterNumber = +counter.textContent;
+    let increment = target / 600;
+
+    if (nowCounterNumber < target) {
+      counter.textContent = `${Math.ceil(nowCounterNumber + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.textContent =
+        target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + `${index == counters.length - 1 ? 'K' : ''}`;
     }
-    started = true;
+  };
+
+  window.addEventListener('scroll', statsScrollTrigger);
+
+  function statsScrollTrigger() {
+    console.log('call statsScrollTrigger');
+
+    if (this.scrollY >= statsSection.offsetTop - 100) {
+      updateCounter();
+      window.removeEventListener('scroll', statsScrollTrigger);
+    }
   }
-};
-
-function startCount(el) {
-  let target = el.dataset.target;
-  let count = setInterval(() => {
-    el.textContent++;
-    if (el.textContent == target) {
-      clearInterval(count);
-    }
-  }, 2000 / target);
-}
-
-// counters.forEach((counter, index) => {
-//   window.addEventListener('scroll', ScrollTrigger);
-
-//   function ScrollTrigger() {
-//     if (window.scrollY >= statsSection.offsetTop - 150) {
-//       updateCounter();
-//       window.removeEventListener('scroll', ScrollTrigger);
-//     }
-//   }
-
-//   function updateCounter() {
-//     // + to number
-//     // https://stackoverflow.com/a/1133814/16107539
-//     let target = +counter.dataset.target;
-//     let nowCounterNumber = +counter.textContent;
-//     let increment = target / 600;
-
-//     if (nowCounterNumber < target) {
-//       counter.textContent = `${Math.ceil(nowCounterNumber + increment)}`;
-//       setTimeout(updateCounter, 1);
-//     } else {
-//       counter.textContent =
-//         target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + `${index == counters.length - 1 ? 'K' : ''}`;
-//     }
-//   }
-// });
+});
 
 // autocomplete="off" ==============================================================================
 let inputs = document.querySelectorAll('.input');
 
 inputs.forEach(element => {
+  console.log('call inputs.forEach');
+
   element.setAttribute('autocomplete', 'off');
 });
 
@@ -60,7 +49,9 @@ inputs.forEach(element => {
 let countDownDate = new Date('Dec 31, 2023 23:59:59').getTime();
 
 let counter = setInterval(() => {
-  // Get Date Now
+  console.log('call counter');
+
+  // Get Date Now -- here cuz milliseconds
   let dateNow = new Date().getTime();
 
   // Find The Date Difference Between Now And Countdown Date
@@ -84,16 +75,25 @@ let counter = setInterval(() => {
 }, 1000);
 
 // progress animation ==============================================================================
-let section = document.querySelector('.our-skills');
+let ourSkillsSection = document.querySelector('.our-skills');
 let progressSpans = document.querySelectorAll('.the-progress span');
 
-window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop + 50) {
-    progressSpans.forEach(span => {
-      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
-      // console.log(span.dataset.width);
-      // console.log(span.getAttribute('data-width'));
-      span.style.width = span.dataset.width;
-    });
+window.addEventListener('scroll', progressScrollTrigger);
+
+function progressScrollTrigger() {
+  console.log('call progressScrollTrigger');
+
+  if (this.scrollY >= ourSkillsSection.offsetTop - 30) {
+    addProgress();
+    window.removeEventListener('scroll', progressScrollTrigger);
   }
-};
+}
+
+function addProgress() {
+  progressSpans.forEach(span => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
+    // console.log(span.dataset.width);
+    // console.log(span.getAttribute('data-width'));
+    span.style.width = span.dataset.width;
+  });
+}
